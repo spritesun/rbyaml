abfb807d227a6ade13f0149b8b0202626aeb7433
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-05-07 02:01:44 +0000

    add spec test for fixing symbol parsing bug

--- spec/rbyaml/_load_spec.rb
+++ spec/rbyaml/_load_spec.rb
@@ -1,4 +1,4 @@
-require '../spec_helper'
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "RbYAML#_load" do
   it "should return a symbol when loading a symbol" do
---
+++ spec/rbyaml/constructor/get_data_spec.rb
@@ -0,0 +1,8 @@
+require File.join(File.dirname(__FILE__), 'fixtures', 'classes')
+
+describe "RbYAML::Constructor#get_data" do
+  it "should return a symbol when getting a symbol stream" do
+    constructor = Constructor.new_by_string("--- :sym")
+    constructor.get_data.should == :sym
+  end
+end
