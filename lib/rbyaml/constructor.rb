f4b299394bfff28fce9dc2d2a791f0eeccd83bb6
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-05-10 19:31:35 +0000

    fix symbol bug in constructor, refact some spec test

--- lib/rbyaml/constructor.rb
+++ lib/rbyaml/constructor.rb
@@ -404,6 +404,10 @@ module RbYAML
       val.empty? ? nil : val
     end
 
+    def construct_yaml_sym(node)
+      str(node).sub(":", "").to_sym
+    end
+    
     def construct_yaml_seq(node)
       seq(node)
     end
@@ -436,6 +440,7 @@ module RbYAML
   SafeConstructor.add_constructor('tag:yaml.org,2002:str',:construct_yaml_str)
   SafeConstructor.add_constructor('tag:yaml.org,2002:seq',:construct_yaml_seq)
   SafeConstructor.add_constructor('tag:yaml.org,2002:map',:construct_yaml_map)
+  SafeConstructor.add_constructor('tag:yaml.org,2002:sym', :construct_yaml_sym)
   SafeConstructor.add_constructor(nil,:construct_private_type)
 
   class Constructor < SafeConstructor
--- spec/rbyaml/composer/compose_document_spec.rb
+++ spec/rbyaml/composer/compose_document_spec.rb
@@ -1,23 +1,24 @@
 require File.join(File.dirname(__FILE__), 'fixtures', 'classes')
 
 describe "RbYAML:Composer#compose_document" do
+  before :each do
+    @sym_composer = RbYAML::Composer.new_by_string(":sym")
+    @int_composer = RbYAML::Composer.new_by_string("12")
+  end
+  
   it "should get string value when compose by symbol string" do
-    composer = RbYAML::Composer.new_by_string(":sym")
-    node = composer.compose_document
-    node.value.should == ":sym"
+    @sym_composer.compose_document.value.should == ":sym"
   end
   
   it "should get sym tag when compose by symbol string" do
-    composer = RbYAML::Composer.new_by_string(":sym")
-    node = composer.compose_document
-    node.tag.should == "tag:yaml.org,2002:sym"
+    @sym_composer.compose_document.tag.should == "tag:yaml.org,2002:sym"
   end
   
   it "should get string value when compose by integer string" do
-    RbYAML::Composer.new_by_string("12").compose_document.value.should == "12"
+    @int_composer.compose_document.value.should == "12"
   end
   
   it "should get int tag when compose by integer string" do
-    RbYAML::Composer.new_by_string("12").compose_document.tag.should == "tag:yaml.org,2002:int"
+    @int_composer.compose_document.tag.should == "tag:yaml.org,2002:int"
   end
 end
