f0f47d3e02e51544d66d3f2baa7eeab80c25ed84
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-06-09 06:51:00 +0000

    Load string which include ">" and "|", according to block scalar headers specification.

--- lib/rbyaml/scanner.rb
+++ lib/rbyaml/scanner.rb
@@ -358,12 +358,12 @@ module RbYAML
       when ?{: return fetch_flow_mapping_start
       when ?]: return fetch_flow_sequence_end
       when ?}: return fetch_flow_mapping_end
-      when ?,: return fetch_flow_entry if @flow_level != 0
+      when ?,: return fetch_flow_entry if !@flow_zero
       when ?*: return fetch_alias
       when ?&: return fetch_anchor
       when ?!: return fetch_tag
-      when ?|: if @flow_zero: return fetch_literal end
-      when ?>: if @flow_zero: return fetch_folded end
+      when ?|: if @flow_zero && CHOMPING.include?(peek1): return fetch_literal end
+      when ?>: if @flow_zero && CHOMPING.include?(peek1): return fetch_folded end
       end
       return fetch_plain if BEG =~ prefix(2) || !NULL_BL_T_LINEBR.include?(peek1)
       raise ScannerError.new("while scanning for the next token","found character #{ch.chr}(#{ch}) that cannot start any token")
