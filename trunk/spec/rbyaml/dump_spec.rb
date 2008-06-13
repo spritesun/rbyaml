3b56ddc736d25b8fbb313551fd43ba5f91259ecc
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-06-13 12:36:28 +0000

    Add dump_to matcher for yaml dump specs.

--- trunk/spec/rbyaml/dump_spec.rb
+++ trunk/spec/rbyaml/dump_spec.rb
@@ -2,21 +2,25 @@
 # Distributed under BSD License
 # Modified by Long Sun
 
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require File.join(File.dirname(__FILE__), "rbyaml_helper")
 
 describe "RbYAML.dump" do
   after :each do
     File.delete $test_file if File.exist? $test_file
   end
-  
-  it "converts an object to RbYAML and write result to io when io provided" do
+
+  it "should converts an object to RbYAML and write result to io when io provided" do
     File.open($test_file, 'w' ) do |io|
       RbYAML.dump( ['badger', 'elephant', 'tiger'], io )
     end
     RbYAML.load_file($test_file).should == ['badger', 'elephant', 'tiger']
   end
-  
-  it "returns a string containing dumped RbYAML when no io provided" do
-    RbYAML.dump( :locked ) == "--- :locked"
-  end  
+
+  it "should returns a string containing dumped RbYAML when no io provided" do
+    :locked.should dump_as("--- :locked\n")
+  end
+
+  it "should dump basic string successfully" do
+    "str".should dump_as("--- str\n")
+  end
 end
