0711598f3f067f9014cc48ba96111b82338983c8
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-07-05 15:16:00 +0000

    Add RbYAMl::parse method

--- trunk/spec/rbyaml/parse_spec.rb
+++ trunk/spec/rbyaml/parse_spec.rb
@@ -1,1 +1,8 @@
+require File.join(File.dirname(__FILE__), "rbyaml_helper")
 
+describe "RbYAML#parse" do
+  it "should parse string text to scalar node" do
+    text = "chenderong"
+    RbYAML.parse(text).should == RbYAML::ScalarNode.new(String.new.taguri, text)
+  end
+end
