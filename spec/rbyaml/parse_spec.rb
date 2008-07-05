c987f4bf1e57c8910d33caa1073b8cc15a11447c
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-07-05 15:59:04 +0000

    Add RbYAML::Node.transform method, it could export object by node which export by parse method.

--- spec/rbyaml/parse_spec.rb
+++ spec/rbyaml/parse_spec.rb
@@ -2,7 +2,6 @@ require File.join(File.dirname(__FILE__), "rbyaml_helper")
 
 describe "RbYAML#parse" do
   it "should parse string text to scalar node" do
-    text = "chenderong"
-    RbYAML.parse(text).should == RbYAML::ScalarNode.new(String.new.taguri, text)
+    RbYAML.parse("---\nchenderong").should == RbYAML::ScalarNode.new(String.new.taguri, "chenderong")
   end
 end
