d45c49a813c84ce5bc37003aac5f6c2a77c8e42f
spritesun <spritesun@f21ceb4e-804b-0410-8f49-193e9cfc9f8b>
2008-10-26 15:24:03 +0000

    merge to_yaml test together

--- spec/hash/to_yaml_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-
-describe "Hash#to_yaml" do
-  it "should has to_yaml method" do
-    Hash.instance_methods(false).should include("to_yaml")
-  end
-end
